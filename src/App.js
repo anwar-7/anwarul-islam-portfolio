b21d687182b1d756da5fa47b28a40f7bdd67ea04
anwarul1 <anwarjewel007@gmail.com>
2022-06-14 13:57:48 +0600

    project anwarul-inslam-portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p class="text-3xl font-bold underline">Anwarul Islam</p>
+      <h1>Anwarul Islam</h1>
     </div>
   );
 }
